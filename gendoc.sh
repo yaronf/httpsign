0e45fe9a215ddf1b9a4e55bc627609ef7c9d45fd
Yaron Sheffer <yaronf@gmx.com>
2022-01-18 00:36:50 +0200

    GH actions

--- gendoc.sh
+++ gendoc.sh
@@ -7,7 +7,7 @@ godoc -url http://localhost:6060/lib/godoc/godocs.js > ./lib/godoc/godocs.js
 godoc -url http://localhost:6060/lib/godoc/jquery.js > ./lib/godoc/jquery.js
 godoc -url http://localhost:6060/lib/godoc/style.css > ./lib/godoc/style.css
 
-# Generate the doc (the first line generated is garbage)
+# Generate the doc (the first line generated is garbage, removed with "tail")
 export pkg="httpsign"
 godoc -url http://localhost:6060/pkg/$pkg | tail -n +2 > $pkg.html
 
